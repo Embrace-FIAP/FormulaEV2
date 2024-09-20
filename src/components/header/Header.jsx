1e7d17b2d2ade574e3656b8b5f1da2cf1f8f4fb0
YuriLopess <costalopesyuri@gmail.com>
2024-09-20 19:25:20 -0300

    Improvement: improve header structure.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/image/logo.png';
 import './header.css';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="Navbar">
-      <a href="/" className="nav-logo">Formula E</a>
-      <nav className={`nav-items ${isOpen && "open"}`}>
+    <header>
+      <img src={logo} alt="logo" id='logo'/>
+      <nav className={`navItems ${isOpen && "open"}`}>
         <a href="/home">Home</a>
         <a href="/about">About</a>
         <a href="/service">Leaderboard</a>
         <a href="/contact">Contact</a>
       </nav>
       <button
-        className={`nav-toggle ${isOpen && "open"}`}
+        className="loginButton"
+        onClick={() => alert('Login clicked!')}>
+        Login
+      </button>
+      <button
+        className="navToggle"
         onClick={() => setIsOpen(!isOpen)}
         aria-label="Menu Toggle"
       >
-        <div className="bar"></div>
+        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className="customIcon" />
       </button>
     </header>
   );
