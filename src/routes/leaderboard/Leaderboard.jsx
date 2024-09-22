23cd3b0e21086d6dbb8c3ba620159db025c5274c
YuriLopess <costalopesyuri@gmail.com>
2024-09-22 14:16:21 -0300

    Feature: Add inital structure in footer component.

--- src/routes/leaderboard/Leaderboard.jsx
+++ src/routes/leaderboard/Leaderboard.jsx
@@ -3,6 +3,30 @@ import React from 'react'
 const Leaderboard = () => {
     return (
         <>
+            <section>
+                <div className='headerTitle'>
+                    <h1>Placar de Líderes</h1>
+                    <p>Saiba quem são os maiores vencedores e qual sua posição no placar.</p>
+                </div>
+
+                <div>
+                    <div>
+
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                        
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
